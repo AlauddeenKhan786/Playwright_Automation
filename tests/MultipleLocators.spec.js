931fd694e7ed1fd19cc1c38aedfb14c0ff091ba9
AlauddeenKhan786 <alauddeenkhan786@gmail.com>
2026-09-12 19:47:07 +0530

    Delete tests/MultipleLocators.spec.js
    unnecessary file

--- tests/MultipleLocators.spec.js
+++
@@ -1,9 +0,0 @@
-const{test , expect} = require('@playwright/test')
-
-//import{test , expect } from '@playwright/test'
-
-test('Multiple Locators' , async({page})=>
-{
-    await page.goto('https://demoblaze.com/')
-
-})
